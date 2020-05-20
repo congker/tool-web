2faff995a741207849b6d31f69bf1d49d95173cf
root <994481905@qq.com>
2020-05-20 11:49:35 +0800

    更新脚本文件

--- shell/deploy-web.sh
+++ shell/deploy-web.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-# 发布 server 下应用，运行的jar从god-server-base任务copy，需先执行god-server-base任务生成jar。
+# 部署微服务脚本，
 OPTION=$1
 PROJECT=$2
 PROFILE=$3
 MONITOR_PORT=$4
-WITH_NGINX=$5
-
 CURRENT_USER=$(whoami)
 pid=0
-nginx_pid=0
 
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-export JAVA_OPTS="-Xms4096m -Xmx4096m"
+export JAVA_OPTS="-Xms2048m -Xmx2048m"
 export JAVA_OPTS_TEST="-Xms512m -Xmx512m"
 export PROJECT_HOME=/home/workspace/ntesgod
-export APP_USER="www-data"
+export APP_USER="winsonxu"
 export VERSION=1.0.0-SNAPSHOT
-
 LANG=en_US.UTF-8
 export LANG
 
@@ -29,96 +25,47 @@ copy() {
   # 应用的jar包存放的目录
   APP_DIR=${PROJECT_HOME}/${DIR}/
   mkdir -p ${APP_DIR}
-  # 测试环境
-  if [[ "${PROFILE}" != "test" ]]; then
-    cp ${PROJECT_HOME}/source/${DIR}/target/${JAR_NAME} ${APP_DIR}
-    echo "[COPY] ${PROJECT_HOME}/source/${DIR}/target/${JAR_NAME} to ${APP_DIR}"
-  else
-    cp ${PROJECT_HOME}/source/${JAR_NAME} ${APP_DIR}
-    echo "[COPY] ${PROJECT_HOME}/source/${JAR_NAME} to ${APP_DIR}"
-  fi
+  cp ${PROJECT_HOME}/source/${PROJECT}/target/${JAR_NAME} ${APP_DIR}
+  echo "[COPY] ${PROJECT_HOME}/source/${PROJECT}/target/${JAR_NAME} ${APP_DIR}"
 }
 
 get_pid() {
   APP=$1
   JAR_NAME=${APP#*.}-${VERSION}.jar
-  jps_result=$(${JAVA_HOME}/bin/jps -ml | grep " ${JAR_NAME}")
+  jps_result=$(${JAVA_HOME}/bin/jps -ml | grep ${JAR_NAME} | grep active=${PROFILE})
+
   if [[ -n "$jps_result" ]]; then
+    echo "[GET_PID] of ${APP}: ${jps_result};"
     pid=$(echo ${jps_result} | awk '{print $1}')
   else
     pid=0
-  fi
-}
-
-get_nginx_pid() {
-  ps_result=$(ps aux | grep nginx | grep master)
-  if [[ -n "$ps_result" ]]; then
-    nginx_pid=$(echo ${ps_result} | awk '{print $2}')
-  else
-    nginx_pid=0
-  fi
-}
-start_nginx() {
-  if [[ "${WITH_NGINX}" != "true" ]]; then
-    return
-  fi
-  get_nginx_pid
-  if [[ ${nginx_pid} -ne 0 ]]; then
-    echo "[START_NGINX] WARN: nginx already started! (pid=$nginx_pid)"
-  else
-    sudo /home/nginx/sbin/nginx
-    sleep 1
-    get_nginx_pid
-    if [[ ${nginx_pid} -ne 0 ]]; then
-      echo "『START_NGINX』 ...(pid=${nginx_pid}) SUCCESS"
-    else
-      echo "『START_NGINX』Failed"
-    fi
-  fi
-}
-
-stop_nginx() {
-  if [[ "${WITH_NGINX}" != "true" ]]; then
-    return
-  fi
-  get_nginx_pid
-  if [[ ${nginx_pid} -ne 0 ]]; then
-    nid=${nginx_pid}
-    count=0
-    sudo /home/nginx/sbin/nginx -s quit
-    while :; do
-      sleep 1
-      get_nginx_pid
-      if [[ ${nginx_pid} -eq 0 ]]; then
-        echo "『STOP_NGINX』...(pid=$nid) SUCCESS"
-        return
-      else
-        let count++
-        if [[ ${count} -gt 20 ]]; then
-          echo "『STOP_NGINX』...(pid=$nid) Failed"
-          return 1
-        fi
-      fi
-    done
+    echo "[GET_PID] of ${APP}: not started."
   fi
 }
 
 check_server_health() {
+  # shellcheck disable=SC2034
   count=0
   health_url="http://127.0.0.1:${MONITOR_PORT}/health"
   echo "[check_health] [${health_url}] blocking..........."
   while :; do
+    # shellcheck disable=SC1083
     http_code=$(curl -I -m 3 -o /dev/null -s -w %{http_code} "${health_url}")
     if [[ ${http_code} -eq 200 ]]; then
+      echo "[check_health] finish"
       return
-    else
-      sleep 3
-      let count++
-      if [[ ${count} -gt 20 ]]; then
-        return 1
-      fi
     fi
+    sleep 3
   done
+}
+
+# 下线微服务
+pause_server() {
+  pause_url="http://127.0.0.1:${MONITOR_PORT}/pause"
+  echo "[pause_server] [${pause_url}] "
+  result=$(curl -X POST -m 5 -s "${pause_url}")
+  echo "[pause_server] result:${result}.blocking 30s............"
+  sleep 30
 }
 
 start() {
@@ -130,43 +77,41 @@ start() {
   if [[ ${pid} -ne 0 ]]; then
     echo "WARN: $APP already started! (pid=$pid)"
   else
-    echo -n "『STARTING』 ${APP} ......"
+    echo -n "[START] ${APP} :"
     JAVA_OPTS_RUNNING=${JAVA_OPTS}
     if [[ "${PROFILE}" == "test" ]]; then
       JAVA_OPTS_RUNNING=${JAVA_OPTS_TEST}
     fi
     cd ${APP_DIR} && nohup ${JAVA_HOME}/bin/java ${JAVA_OPTS_RUNNING} -jar ${JAR_NAME} --spring.profiles.active=${PROFILE} >nohup.log 2>&1 &
+
     if [[ $? -eq 0 ]]; then
       echo -e " [OK]"
-      if [[ "${MONITOR_PORT}" != "0" ]]; then
-        check_server_health
-      fi
-      if [[ $? -eq 0 ]]; then
-        get_pid ${APP}
-        echo "『RELEASE_SUCCESS』: $APP (PID=$pid)"
-        start_nginx
-      else
-        echo "『RELEASE_FAIL』: $APP"
-      fi
     else
       echo -e " [Failed]"
+    fi
+    check_server_health
+    if [[ $? -eq 0 ]]; then
+      get_pid ${APP}
+      echo "『RELEASE_SUCCESS』: $APP (PID=$pid)"
+    else
+      echo "『RELEASE_FAIL』: $APP"
     fi
   fi
 }
 
 stop() {
   APP=$1
-  stop_nginx
   get_pid ${APP}
   if [[ ${pid} -ne 0 ]]; then
-    echo -n "『STOP』 $APP ...(pid=$pid) "
+    echo "[STOP] $APP ...(pid=$pid) "
+    pause_server
     kill ${pid}
     if [[ $? -eq 0 ]]; then
-      echo -e " [OK]"
-      sleep 3
+      echo "[STOP] OK"
     else
-      echo -e " [Failed]"
+      echo "[STOP] FAIL"
     fi
+    sleep 5
   else
     echo "WARN: $APP is not running"
   fi
@@ -179,7 +124,6 @@ if [[ "${CURRENT_USER}" != "${APP_USER}" ]]; then
 fi
 
 echo "================================================================"
-
 case "$OPTION" in
 'start')
   start ${PROJECT}
